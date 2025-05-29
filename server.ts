6c82bb2f0a1eb5e50108b6c911d46d0f7ff10721
madhu <madhubabu@gmail.com>
2025-05-29 11:14:50 +0530

    changecorsconfg

--- server.ts
+++ server.ts
@@ -12,9 +12,23 @@ const PORT = process.env.PORT || 3220;
 // Connect to your database
 connectDB();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://merntypefrontendlogin.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, curl, postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
